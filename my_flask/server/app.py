e33a51f7f5f63d1f35b8cd7cf30e9b11743ef9b0
montanoj96 <montanojoel68@gmail.com>
2023-03-24 14:40:12 -0400

    added additional html for results

--- my_flask/server/app.py
+++ my_flask/server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,9 +10,9 @@ def invest_calculator():
 
 @app.route('/form', methods=["POST"])
 def form():
-    income = request.method #int(input("Enter Income"))
-    expense = 0 #int(input("Enter Expence"))
-    principal = 0
+    income = request.form.get("income") #int(input("Enter Income"))
+    expense = request.form.get("expenses") #int(input("Enter Expence"))
+    principal = request.form.get("principal")
     percent_income = 0 
     if request.method == 'POST':
         if request.form['percent_income'] == '7':
@@ -33,8 +33,8 @@ def form():
             rate_of_interest = 20
     increment = 0
     principal = 0
-    
-    
+
+
     accumulated_value = 0
     years = []
     yearly_earning = []
@@ -42,3 +42,10 @@ def form():
         accumulated_value += (principal+(increment * yearly_contribution))*((1+(rate_of_interest/12))**(12*1))
         yearly_earning.append(accumulated_value) 
         yearlys.append(i) 
+    
+    outputs = zip(years, yearly_earning) 
+    ouput_length = len(output)
+    
+    return render_template("calculation.html", len = len(outputs), calculations = outputs)
+
+    
